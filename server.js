6580ac7024210fa11493d67400e87612bd35a6e8
Vikashkumawat979 <kumawatvikash816@gmail.com>
2026-09-04 17:28:16 +0530

    Remove duplicate path import

--- server.js
+++ server.js
@@ -22,8 +22,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const path = require('path');
-
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.get('/', (req, res) => {
